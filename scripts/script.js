ff86e66a1c8deb5ab3337922b058b52c54fcd393
Law Yang <lawtouayang@gmail.com>
2018-05-18 18:10:10 -0500

    framewire with buttons table only inputs fields working

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,10 @@
 console.log('JS');
-$( document ).ready( readyNow );
+$(document).ready(readyNow);
 
-function readyNow(){
+function readyNow() {
     console.log('JQ ready');
 
-class employee {
-    constructor(first, last, id, title, salary) {
-        this.first = first;
-        this.last = last;
-        this.id = id;
-        this.title = title;
-        this.salary = salary;
-    } // end constructor
-} // end employee class
-
-
+    $('#addButton').on('click', addEmployee)
 
 
 
@@ -28,3 +18,33 @@ class employee {
 
 
 } ///////////////////////////////////END READY NOW
+
+class Employee {
+    constructor(first, last, id, title, salary) {
+        this.first = first;
+        this.last = last;
+        this.id = id;
+        this.title = title;
+        this.salary = salary;
+    } // end constructor
+} // end employee class
+
+let arrayEmployee = [];
+
+function addEmployee() {
+    console.log('test test');
+    // get user input
+    // create new employee from input
+    let newEmployee = new Employee($('#firstName').val, $('#lastName').val, $('#employId').val(), $('#employTitle').val(), $('employSalary').val());
+    console.log(newEmployee);
+    // store new employee
+    // push into array
+    arrayEmployee.push(newEmployee)
+    // update DOM
+    //end
+} //end 
+
+
+function displayToTable() {
+
+}
