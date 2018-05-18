60aca94df776ac6d7a18bc06b63cc5afe59155e7
Law Yang <lawtouayang@gmail.com>
2018-05-18 17:19:28 -0500

    initial push with framewire

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+console.log('JS');
+$( document ).ready( readyNow );
+
+function readyNow(){
+    console.log('JQ ready');
+
+class employee {
+    constructor(first, last, id, title, salary) {
+        this.first = first;
+        this.last = last;
+        this.id = id;
+        this.title = title;
+        this.salary = salary;
+    } // end constructor
+} // end employee class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} ///////////////////////////////////END READY NOW
