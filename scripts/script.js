60f2e5b513955a3c5fc2d8fa5c7ad3a13748c86b
Law Yang <lawtouayang@gmail.com>
2018-05-19 00:56:55 -0500

    table working, append tables from button working, need to add total annual salary

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,12 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('JQ ready');
 
-    $('#addButton').on('click', addEmployee)
+    $('#addButton').on('click', addEmployee);
+    $('#addButton').on('click', displayToTable);
 
 
 
-
-
-
-
-
-
-
-
-
+    
 } ///////////////////////////////////END READY NOW
 
 class Employee {
@@ -31,20 +24,27 @@ class Employee {
 
 let arrayEmployee = [];
 
+// function adds created employee class to arrayEmployee array
 function addEmployee() {
-    console.log('test test');
     // get user input
     // create new employee from input
-    let newEmployee = new Employee($('#firstName').val, $('#lastName').val, $('#employId').val(), $('#employTitle').val(), $('employSalary').val());
+    let newEmployee = new Employee( $('#firstName').val(), $('#lastName').val(), $('#employId').val(), $('#employTitle').val(), $('#employSalary').val() );
     console.log(newEmployee);
     // store new employee
     // push into array
-    arrayEmployee.push(newEmployee)
+    arrayEmployee.push(newEmployee);
+    return arrayEmployee;
     // update DOM
     //end
 } //end 
 
-
+// function adds created employee to table on DOM
 function displayToTable() {
+    let employeeFirstName = $('#firstName').val();
+    let employeeLastName = $('#lastName').val();
+    let employeeIdNumber = $('#employId').val();
+    let employeeTitleName = $('#employTitle').val();
+    let employeePay = $('#employSalary').val();
 
-}
+    let addRow = $('#employeeTable').append('<tr><td>' + employeeFirstName + '</td> <td>' + employeeLastName + '</td> <td>' + employeeIdNumber + '</td> <td>' + employeeTitleName + '</td> <td>' + employeePay + '</td> </tr>');
+} //end displayToTable function
