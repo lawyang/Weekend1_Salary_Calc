f395e9c2759ee2961cc290120e83008cfcfea9c2
Law Yang <lawtouayang@gmail.com>
2018-05-19 09:31:09 -0500

    all functions, features working. need to format design

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,6 @@ function readyNow() {
     console.log('JQ ready');
 
     $('#addButton').on('click', addEmployee);
-    $('#addButton').on('click', totalAnnualSalary);
-
-
-
 
 } ///////////////////////////////////END READY NOW
 
@@ -33,13 +29,19 @@ function addEmployee() {
     let employeeTitleName = $('#employTitle').val(); //to append into html table
     let employeePay = $('#employSalary').val(); //to append into html table
 
-    let newEmployee = new Employee( $('#firstName').val(), $('#lastName').val(), $('#employId').val(), $('#employTitle').val(), $('#employSalary').val() ); // create new employee upon button click event
+    let newEmployee = new Employee( $('#firstName').val(), $('#lastName').val(), $('#employId').val(), $('#employTitle').val(), parseInt( $('#employSalary').val() ) ); // create new employee upon button click event
+
+    let x = parseInt( $('#employSalary').val() ); // empty value to dump employeeSalary
+    annualSalary += x; // sum of all employee salary
+    console.log(annualSalary); // log total annual salary to check if script is working
+    $('#totalSalary').empty(); // emppty out the totalSalary table row from the DOM
+    $('#totalSalary').append( '<th> Total Annual Salary: ' + '$' + annualSalary + '</th>' ); // append the total annual salary to the emptied table row on he DOM
 
     if ( $('#firstName').val() === "" || $('#lastName').val() === "" || $('#employId').val() === "" || $('#employTitle').val() === "" || $('#employSalary').val() === "" ) {
         alert('Please complete all fields'); // validate if all fields are filled before continuing
     } else {
         // append created employee to a new table row
-        let addRow = $('#employeeTable').append('<tr><td>' + employeeFirstName + '</td> <td>' + employeeLastName + '</td> <td>' + employeeIdNumber + '</td> <td>' + employeeTitleName + '</td> <td>' + employeePay + '</td>   </tr>');
+        let addRow = $('#employeeTable').append('<tr><td>' + employeeFirstName + '</td> <td>' + employeeLastName + '</td> <td>' + employeeIdNumber + '</td> <td>' + employeeTitleName + '</td> <td>' + '$' + employeePay + '</td>   </tr>');
         // log newEmployee array
         console.log(newEmployee);
         // push newly created employees into newEmployee array
@@ -48,23 +50,8 @@ function addEmployee() {
     } // end else statement
 } // end addEmployee function
 
-function totalAnnualSalary (){
-    let x = 0
 
-    for (salary of a {
-        parseInt(arrayEmployee)
-    }
-
-    annualSalary += x;
-    console.log(annualSalary);
-    $('#totalSalary').empty();
-    $('#totalSalary').append( '<th> Total Annual Salary: ' + annualSalary + '</th>' );
-}
-
-
-
-
-//////////////////CODE BEFORE CONCATENATING
+//////////////////CODE BEFORE CONCATENATING//////////////////////////////
 // // function adds created employee class to arrayEmployee array
 // function addEmployee() {
 //     // get user input
