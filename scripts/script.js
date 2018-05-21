2e1fbf999f26d681d192ebb3d0ce251a53fee359
Law Yang <lawtouayang@gmail.com>
2018-05-20 22:12:48 -0500

    base mode completed except for clearing input fields. still need to do hard mode and pro mode

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,63 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('JQ ready');
 
-    $('#addButton').on('click', addEmployee);
+    let arrayEmployee = [];
+    let annualSalary = 0;
+    let monthlyPay = 0;
+    let remove = '<td><button id="deleteButton"> Delete </button></td>'; 
 
-    // over 20000
-    if (annualSalary > 20000){
-        
-    }
+    $('#addButton').on('click', function( ){
+        let employeeFirstName = $('#firstName').val(); //to append into html table
+        let employeeLastName = $('#lastName').val(); //to append into html table
+        let employeeIdNumber = $('#employId').val(); //to append into html table
+        let employeeTitleName = $('#employTitle').val(); //to append into html table
+        let employeePay = $('#employSalary').val(); //to append into html table
+        let newEmployee = new Employee( $('#firstName').val(), $('#lastName').val(), $('#employId').val(), $('#employTitle').val(), parseInt( $('#employSalary').val() ) ); // create new employee upon button click event
+         
 
-} ///////////////////////////////////END READY NOW
+        if ( $('#firstName').val() === "" || $('#lastName').val() === "" || $('#employId').val() === "" || $('#employTitle').val() === "" || $('#employSalary').val() === "" ) {
+            $('#addButton').prop('diabled', true );
+            alert('Please fill all fields'); // validate if all fields are filled before continuing
+        } else {
+            $('#addButton').prop('diabled', false );
+
+            // append created employee to a new table row
+            let addRow = $('#employeeTable').append('<tr><td>' + employeeFirstName + '</td> <td>' + employeeLastName + '</td> <td>' + employeeIdNumber + '</td> <td>' + employeeTitleName + '</td> <td>' + '$' + employeePay + remove + '</tr>');
+            // log newEmployee array
+            console.log(newEmployee);
+            arrayEmployee.push(newEmployee); // push newemployee into arrayEmployee 
+            
+            let x = parseInt( $('#employSalary').val() ); // empty value to dump employeeSalary
+            annualSalary += x; // sum of all employee salary
+            console.log('total annual', annualSalary); // log total annual salary to check if script is working
+            monthlyPay = annualSalary / 12; // dive annual salary by 12
+            console.log('total monthly', monthlyPay); // log total annual salary to check if script is working
+            $('#totalSalary').empty(); // empty out the totalSalary table row from the DOM
+            $('#totalSalary').append( '<div> Total Annual Salary: ' + '$' + monthlyPay.toFixed(2) + '</div>' ); // append the total annual salary to the emptied table row on he DOM
+            return arrayEmployee;
+        } // end else statement
+    });
+
+    $('#addButton').on('click', function( ) {
+        if (monthlyPay > 20000){
+            console.log('over');
+            alert('Monthly salary is over $20,000');
+            $('#sumTable th').css("background-color", "red");
+            }
+        });
+
+    $('remove').on('click', function( ){
+        alert('HI');
+    });
+
+
+    
+    
+    
+    
+    
+    
+    }///////////////////////////////////END READY NOW//////////////////////////
 
 class Employee {
     constructor(first, last, id, title, salary) {
@@ -23,92 +72,10 @@ class Employee {
     } // end constructor
 } // end employee class
 
-let arrayEmployee = [];
-let annualSalary = 0;
-
-function addEmployee() {
-    let employeeFirstName = $('#firstName').val(); //to append into html table
-    let employeeLastName = $('#lastName').val(); //to append into html table
-    let employeeIdNumber = $('#employId').val(); //to append into html table
-    let employeeTitleName = $('#employTitle').val(); //to append into html table
-    let employeePay = $('#employSalary').val(); //to append into html table
-    let newEmployee = new Employee( $('#firstName').val(), $('#lastName').val(), $('#employId').val(), $('#employTitle').val(), parseInt( $('#employSalary').val() ) ); // create new employee upon button click event
-
-    if ( $('#firstName').val() === "" || $('#lastName').val() === "" || $('#employId').val() === "" || $('#employTitle').val() === "" || $('#employSalary').val() === "" ) {
-        $('#addButton').prop('diabled', true );
-        alert('Please fill all fields'); // validate if all fields are filled before continuing
-    } else {
-        $('#addButton').prop('diabled', false );
-
-        // append created employee to a new table row
-        let addRow = $('#employeeTable').append('<tr><td>' + employeeFirstName + '</td> <td>' + employeeLastName + '</td> <td>' + employeeIdNumber + '</td> <td>' + employeeTitleName + '</td> <td>' + '$' + employeePay + '</td>   </tr>');
-        // log newEmployee array
-        console.log(newEmployee);
-        arrayEmployee.push(newEmployee); // push newemployee into arrayEmployee 
-        let x = parseInt( $('#employSalary').val() ); // empty value to dump employeeSalary
-        annualSalary += x; // sum of all employee salary
-        console.log(annualSalary); // log total annual salary to check if script is working
-        $('#totalSalary').empty(); // empty out the totalSalary table row from the DOM
-        $('#totalSalary').append( '<div> Total Annual Salary: ' + '$' + annualSalary.toFixed(2) + '</div>' ); // append the total annual salary to the emptied table row on he DOM
-        return arrayEmployee;
-    } // end else statement
-} // end addEmployee function
-
 function clear() {
     $('#firstName').val(''); 
     $('#lastName').val('');
     $('#employId').val('');
     $('#employTitle').val('');
     $('#employSalary').val('');
-}
-
-
-
-// function totalAnnualSalary() {
-//     let x = parseInt( $('#employSalary').val() ); // empty value to dump employeeSalary
-//     annualSalary += x; // sum of all employee salary
-//     console.log(annualSalary); // log total annual salary to check if script is working
-//     $('#totalSalary').empty(); // empty out the totalSalary table row from the DOM
-//     $('#totalSalary').append( '<div> Total Annual Salary: ' + '$' + annualSalary + '</div>' ); // append the total annual salary to the emptied table row on he DOM
-// }
-
-
-
-// function turnRed() {
-//     if (annualSalary > 20000) {
-//         $('#overAmount').css("background-color", "red");
-//     }
-// }
-
-
-// $('#firstName').val(''); 
-// $('#lastName').val('');
-// $('#employId').val('');
-// $('#employTitle').val('');
-// $('#employSalary').val('');
-
-//////////////////CODE BEFORE CONCATENATING//////////////////////////////
-// // function adds created employee class to arrayEmployee array
-// function addEmployee() {
-//     // get user input
-//     // create new employee from input
-//     let newEmployee = new Employee( $('#firstName').val(), $('#lastName').val(), $('#employId').val(), $('#employTitle').val(), $('#employSalary').val() );
-//     console.log(newEmployee);
-//     // store new employee
-//     // push into array
-//     arrayEmployee.push(newEmployee);
-//     return arrayEmployee;
-//     // update DOM
-//     //end
-// } //end 
-
-// function adds created employee to table on DOM
-// function displayToTable() {
-//     let employeeFirstName = $('#firstName').val();
-//     let employeeLastName = $('#lastName').val();
-//     let employeeIdNumber = $('#employId').val();
-//     let employeeTitleName = $('#employTitle').val();
-//     let employeePay = $('#employSalary').val();
-
-//     let addRow = $('#employeeTable').append('<tr><td>' + employeeFirstName + '</td> <td>' + employeeLastName + '</td> <td>' + employeeIdNumber + '</td> <td>' + employeeTitleName + '</td> <td>' + employeePay + '</td> </tr>');
-// } //end displayToTable function
+} // end clear function
